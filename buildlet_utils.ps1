b9cce9302fcf126d05ddd5368de7cda10264c7a5
Jon <jon.forums@gmail.com>
2013-03-22 00:13:01 -0400

    Teach Fetch-Archive how to do FTP downloads

--- buildlet_utils.ps1
+++ buildlet_utils.ps1
@@ -2,7 +2,7 @@
 
 # Author: Jon Maken
 # License: 3-clause BSD
-# Revision: 2013-03-21 22:49:38 -0600
+# Revision: 2013-03-22 00:19:57 -0600
 
 function Write-Status($msg, $leader='--->', $color='Yellow') {
   Write-Host "$leader $msg" -foregroundcolor $color
@@ -10,9 +10,45 @@ function Write-Status($msg, $leader='--->', $color='Yellow') {
 
 function Fetch-Archive() {
   if (-not (Test-Path $source)) {
-    Import-Module BitsTransfer
     Write-Status "downloading $archive"
-    Start-BitsTransfer $archive "$PWD\$source"
+    $uri = New-Object System.URI $archive
+
+    switch -regex ($uri.Scheme.ToLower()) {
+      '^(?:http|https)' {
+        Import-Module BitsTransfer
+        Start-BitsTransfer $archive "$PWD\$source"
+        break
+      }
+      '^ftp$' {
+        try {
+          [System.Net.FtpWebRequest]$request = [System.Net.WebRequest]::Create($archive)
+          $request.Method = [System.Net.WebRequestMethods+Ftp]::DownloadFile
+          $request.UseBinary = $true
+          $request.UsePassive = $true
+
+          $response = $request.GetResponse()
+          $response_stream = $response.GetResponseStream()
+          $fs = New-Object System.IO.FileStream "$PWD/$source", 'Create', 'Write'
+
+          [byte[]] $buffer = New-Object byte[] 4096
+
+          do {
+            $count = $response_stream.Read($buffer, 0, $buffer.Length)
+            $fs.Write($buffer, 0, $count)
+          } while ($count -gt 0)  # EOS when Read returns 0 bytes
+        }
+        catch {
+          throw "[ERROR] failure trying to perform FTP download"
+        }
+        finally {
+          if ($fs) { $fs.Flush(); $fs.Close() }
+          if ($response_stream) { $response_stream.Close() }
+          if ($response) { $response.Close() }
+        }
+        break
+      }
+      default { throw "[ERROR] unsupported network scheme: $($uri.Scheme.ToLower())" }
+    }
   }
 }
 
