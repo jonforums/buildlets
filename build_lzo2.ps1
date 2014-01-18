e11b59977b8d43b43d92e2f950fb985ed14f3ba0
Jon <jon.forums@gmail.com>
2014-01-18 02:04:28 -0500

    Add minilzo support to lzo2 buildlet

--- build_lzo2.ps1
+++ build_lzo2.ps1
@@ -2,7 +2,7 @@
 
 # Author: Jon Maken
 # License: 3-clause BSD
-# Revision: 2014-01-18 00:23:58 -0600
+# Revision: 2014-01-18 01:56:11 -0600
 
 param(
   [parameter(Mandatory=$true,
@@ -56,10 +56,16 @@ Push-Location "${source_dir}"
   # build
   New-Build {
     sh -c "make" | Out-Null
+
+    # build static minilzo library
+    sh -c "gcc -g -Wall -Wextra -O2 -Iinclude/lzo -c minilzo/minilzo.c -o minilzo/minilzo.o" | Out-Null
+    sh -c "ar rcs minilzo/libminilzo.a minilzo/minilzo.o" | Out-Null
   }
 
   # install
   sh -c "make install" | Out-Null
+  cp "minilzo/libminilzo.a" "${install_dir}/lib" | Out-Null
+  cp "minilzo/minilzo.h" "${install_dir}/include/lzo" | Out-Null
 
   # archive
   Archive-Build
