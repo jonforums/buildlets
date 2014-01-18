a7a60c79906afb51f34f48756cbf58bbca5ce5f9
Jon <jon.forums@gmail.com>
2014-01-18 01:24:31 -0500

    Add lzo2 buildlet

---
+++ build_lzo2.ps1
@@ -0,0 +1,70 @@
+#requires -version 3.0
+
+# Author: Jon Maken
+# License: 3-clause BSD
+# Revision: 2014-01-18 00:23:58 -0600
+
+param(
+  [parameter(Mandatory=$true,
+             Position=0,
+             HelpMessage='lzo2 version to build (eg - 2.06)')]
+  [validateset('2.06')]
+  [alias('v')]
+  [string] $version,
+
+  [parameter(HelpMessage='perform a 64-bit build')]
+  [switch] $x64
+)
+
+$libname = 'lzo'
+$source = "${libname}-${version}.tar.gz"
+$source_dir = "${libname}-${version}"
+$repo_root = "http://www.oberhumer.com/opensource/lzo/download/"
+$archive = "${repo_root}${source}"
+$hash_uri = "https://raw.github.com/jonforums/buildlets/master/hashery/${libname}.sha1"
+
+# source the buildlet library
+. "$PWD\buildlet_utils.ps1"
+
+# download source archive
+Fetch-Archive
+
+# download hash data and validate source archive
+Validate-Archive
+
+# extract
+Extract-Archive
+
+# patch, configure, build, archive
+Push-Location "${source_dir}"
+
+  # activate toolchain
+  Activate-Toolchain
+
+  # configure
+  Configure-Build {
+    # FIXME config.guess cannot guess system type
+    if ($x64) {
+      $triplets = '--build=x86_64-w64-mingw32'
+    } else {
+      $triplets = '--build=i686-w64-mingw32'
+    }
+
+    sh -c "./configure --prefix=${install_dir} ${triplets}" | Out-Null
+  }
+
+  # build
+  New-Build {
+    sh -c "make" | Out-Null
+  }
+
+  # install
+  sh -c "make install" | Out-Null
+
+  # archive
+  Archive-Build
+
+Pop-Location
+
+# cleanup
+Clean-Build
