353f80d5817ba1102116e6fa327629c9f9785b62
Jon <jon.forums@gmail.com>
2014-04-04 23:07:36 -0400

    Update SQLite buildlet to support 3.8.4.3
    Remove build support for the following versions:
    
      3.8.3.1
      3.8.4.1
      3.8.4.2

--- build_sqlite.ps1
+++ build_sqlite.ps1
@@ -2,13 +2,13 @@
 
 # Author: Jon Maken
 # License: 3-clause BSD
-# Revision: 2014-03-27 22:05:14 -0600
+# Revision: 2014-04-04 22:57:46 -0600
 
 param(
   [parameter(Mandatory=$true,
              Position=0,
-             HelpMessage='sqlite version to build (eg - 3.8.4.1).')]
-  [validateset('3.8.2','3.8.3.1','3.8.4.1','3.8.4.2')]
+             HelpMessage='sqlite version to build (eg - 3.8.4.3).')]
+  [validateset('3.8.2','3.8.4.3')]
   [alias('v')]
   [string] $version,
 
@@ -21,9 +21,7 @@ param(
 $sqlite_version = $v[0]*1000000 + $v[1]*10000 + $v[2]*100
 if ($v.Length -eq 4) { $sqlite_version += $v[3] }
 $sqlite_dirs = @{'3.8.2' = '2013';
-                 '3.8.3.1' = '2014';
-                 '3.8.4.1' = '2014';
-                 '3.8.4.2' = '2014'}
+                 '3.8.4.3' = '2014'}
 
 $libname = 'sqlite'
 $source = "${libname}-amalgamation-${sqlite_version}.zip"
