83d52d39769f1cb145811317dd9265ff1f70e74c
Jon <jon.forums@gmail.com>
2015-01-08 11:56:26 -0500

    Add libsodium buildlet

---
+++ build_libsodium.ps1
@@ -0,0 +1,65 @@
+#requires -version 3.0
+
+# Author: Jon Maken
+# License: 3-clause BSD
+# Revision: 2015-01-08 11:26:37 -0600
+
+param(
+  [parameter(Mandatory=$true,
+             Position=0,
+             HelpMessage='libsodium version to build (eg - 1.0.1)')]
+  [validateset('1.0.1')]
+  [alias('v')]
+  [string] $version,
+
+  [parameter(HelpMessage='perform a 64-bit build')]
+  [switch] $x64
+)
+
+$libname = 'libsodium'
+$source = "${libname}-${version}.tar.gz"
+$source_dir = "${libname}-${version}"
+$repo_root = "https://download.${libname}.org/${libname}/releases/"
+$archive = "${repo_root}${source}"
+$hash_uri = "https://raw.github.com/jonforums/buildlets/master/hashery/${libname}.sha1"
+
+# source the buildlet library
+. "$PWD\buildlet_utils.ps1"
+
+# download source archive
+Fetch-Archive
+
+# download hash data and validate source archive
+Validate-Archive
+
+# extract
+Extract-Archive
+
+# patch, configure, build, archive
+Push-Location "${source_dir}"
+
+  # activate toolchain
+  Activate-Toolchain
+
+  # configure
+  Configure-Build {
+    sh -c "./configure --prefix=${install_dir} ${triplets}" | Out-Null
+  }
+
+  # build
+  New-Build {
+    sh -c "make" | Out-Null
+  }
+
+  # TODO add `make check` step
+
+  # install
+  sh -c "make install" | Out-Null
+
+  # archive
+  Archive-Build
+
+Pop-Location
+
+# cleanup
+Clean-Build
