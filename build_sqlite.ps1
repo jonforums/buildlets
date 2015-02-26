5096876a557175941b725547b54be11d9a2d0183
Jon <jon.forums@gmail.com>
2015-02-25 21:46:04 -0500

    Update SQLite buildlet to support 3.8.8.3

--- build_sqlite.ps1
+++ build_sqlite.ps1
@@ -2,13 +2,13 @@
 
 # Author: Jon Maken
 # License: 3-clause BSD
-# Revision: 2015-01-30 14:22:52 -0600
+# Revision: 2015-02-25 21:36:34 -0600
 
 param(
   [parameter(Mandatory=$true,
              Position=0,
-             HelpMessage='sqlite version to build (eg - 3.8.8.2).')]
-  [validateset('3.8.6','3.8.7.4','3.8.8.2')]
+             HelpMessage='sqlite version to build (eg - 3.8.8.3).')]
+  [validateset('3.8.6','3.8.7.4','3.8.8.3')]
   [alias('v')]
   [string] $version,
 
@@ -22,7 +22,7 @@ $sqlite_version = $v[0]*1000000 + $v[1]*10000 + $v[2]*100
 if ($v.Length -eq 4) { $sqlite_version += $v[3] }
 $sqlite_dirs = @{'3.8.6'   = '2014';
                  '3.8.7.4' = '2014';
-                 '3.8.8.2' = '2015'}
+                 '3.8.8.3' = '2015'}
 
 $libname = 'sqlite'
 $source = "${libname}-amalgamation-${sqlite_version}.zip"
