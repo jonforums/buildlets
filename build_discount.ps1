f0dd87fc9a542a4e28cdeafc0f3455bf5382c8f6
Jon <jon.forums@gmail.com>
2016-11-09 22:22:41 -0500

    Add Discount buildlet supporting 2.2.1

---
+++ build_discount.ps1
@@ -0,0 +1,66 @@
+#requires -version 3.0
+
+# Author: Jon Maken
+# License: 3-clause BSD
+# Revision: 2016-11-09 19:13:11 -0600
+
+param(
+  [parameter(Mandatory=$true,
+             Position=0,
+             HelpMessage='Discount version to build (eg - 2.2.1)')]
+  [validateset('2.2.1')]
+  [alias('v')]
+  [string] $version,
+
+  [parameter(HelpMessage='perform a 64-bit build')]
+  [switch] $x64
+)
+
+$libname = 'discount'
+$source = "${libname}-${version}.tar.gz"
+$source_dir = "${libname}-${version}"
+$repo_root = "https://github.com/Orc/${libname}/archive/"
+$archive = "${repo_root}${version}.tar.gz"
+$hash_uri = "https://raw.github.com/jonforums/buildlets/master/hashery/${libname}.sha1"
+
+# source the buildlet library
+. "$PWD\buildlet_utils.ps1"
+
+# download source archive
+Fetch-Archive
+
+# download hash data and validate source archive
+Validate-Archive
+
+# extract
+Extract-Archive
+
+# patch, configure, build, archive
+Push-Location "${source_dir}"
+
+  # activate toolchain
+  Activate-Toolchain {
+    $env:CC = 'gcc'
+    $env:CFLAGS = '-s -O2'
+  }
+
+  # configure
+  Configure-Build {
+    sh -c "./configure.sh --prefix=${install_dir} --enable-dl-tag --enable-superscript" | Out-Null
+  }
+
+  # build
+  New-Build {
+    sh -c "make" | Out-Null
+  }
+
+  # install
+  sh -c "make install" | Out-Null
+
+  # archive
+  Archive-Build
+
+Pop-Location
+
+# cleanup
+Clean-Build
