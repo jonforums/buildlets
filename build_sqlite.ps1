50d972e4784bd1c2ba85cf5d6d2d0328f8e5e526
Jon <jon.forums@gmail.com>
2016-11-30 21:28:32 -0500

    Update SQLite buildlet to support 3.15.2

--- build_sqlite.ps1
+++ build_sqlite.ps1
@@ -2,13 +2,13 @@
 
 # Author: Jon Maken
 # License: 3-clause BSD
-# Revision: 2016-11-06 11:38:00 -0600
+# Revision: 2016-11-30 21:04:40 -0600
 
 param(
   [parameter(Mandatory=$true,
              Position=0,
-             HelpMessage='sqlite version to build (eg - 3.15.1).')]
-  [validateset('3.14.2','3.15.1')]
+             HelpMessage='sqlite version to build (eg - 3.15.2).')]
+  [validateset('3.14.2','3.15.2')]
   [alias('v')]
   [string] $version,
 
@@ -21,7 +21,7 @@ param(
 $sqlite_version = $v[0]*1000000 + $v[1]*10000 + $v[2]*100
 if ($v.Length -eq 4) { $sqlite_version += $v[3] }
 $sqlite_dirs = @{'3.14.2' = '2016'
-                 '3.15.1' = '2016'}
+                 '3.15.2' = '2016'}
 
 $libname = 'sqlite'
 $source = "${libname}-amalgamation-${sqlite_version}.zip"
