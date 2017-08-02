28610bbae911b7d13080e600173ee6a3d4f5acae
Jon <jon.forums@gmail.com>
2017-08-02 07:51:43 -0400

    Update SQLite buildlet to support 3.20.0

--- build_sqlite.ps1
+++ build_sqlite.ps1
@@ -2,13 +2,13 @@
 
 # Author: Jon Maken
 # License: 3-clause BSD
-# Revision: 2017-06-08 20:45:27 -0600
+# Revision: 2017-08-01 21:01:34 -0600
 
 param(
   [parameter(Mandatory=$true,
              Position=0,
-             HelpMessage='sqlite version to build (eg - 3.19.3).')]
-  [validateset('3.18.0','3.19.3')]
+             HelpMessage='sqlite version to build (eg - 3.20.0).')]
+  [validateset('3.19.3','3.20.0')]
   [alias('v')]
   [string] $version,
 
@@ -20,8 +20,8 @@ param(
 [int[]] $v = $version.Split('.')
 $sqlite_version = $v[0]*1000000 + $v[1]*10000 + $v[2]*100
 if ($v.Length -eq 4) { $sqlite_version += $v[3] }
-$sqlite_dirs = @{'3.18.0' = '2017'
-                 '3.19.3' = '2017'}
+$sqlite_dirs = @{'3.19.3' = '2017'
+                 '3.20.0' = '2017'}
 
 $libname = 'sqlite'
 $source = "${libname}-amalgamation-${sqlite_version}.zip"
